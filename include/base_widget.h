2a5d980dc5ce35d3f63fc9fd4a53028868d00c87
Eessh <eesshkumarjagana@gmail.com>
2023-08-01 14:24:01 +0530

    refactor: base widget

--- include/base_widget.h
+++ include/base_widget.h
@@ -2,11 +2,112 @@
 #define SMOLL_WIDGETS__BASE_WIDGET_H
 
 #include "events.h"
-#include "smoll_context.h"
+#include "internal_context.h"
+#include "internal_events.h"
 #include "types.h"
 
-typedef struct base_widget base_widget;
-typedef struct base_widget_child_node base_widget_child_node;
+/// @brief Base widget.
+typedef struct base_widget
+{
+  /// @brief Widgets's x-coordinate.
+  int16 x;
+
+  /// @brief Widgets's y-coordinate.
+  int16 y;
+
+  /// @brief Widget's width (including padding).
+  uint16 w;
+
+  /// @brief Widget's height (including padding).
+  uint16 h;
+
+  /// @brief Tells if widget should be taken into account while
+  ///        layouting and rendering.
+  bool visible;
+
+  /// @brief Foreground color of widget.
+  ///        Applies to text (if widget conatains any).
+  color foreground;
+
+  /// @brief Background color of widget.
+  color background;
+
+  /// @brief Pointer to derived widget.
+  ///        Casting to derived widget type is required to access it.
+  void* derived;
+
+  /// @brief Parent of this widget.
+  base_widget* parent;
+
+  /// @brief Head of children linked list.
+  base_widget_child_node* children_head;
+
+  /// @brief Context of this widget.
+  internal_context* context;
+
+  /// @brief Internal callback for getting bounding rectangle of widget.
+  ///        Will be handy when layouting.
+  rect (*internal_get_bounding_rect_callback)(base_widget*);
+
+  /// @brief Internal callback for adjusting layout of this widget.
+  result_bool (*internal_fit_layout_callback)(base_widget*);
+
+  /// @brief Internal callback for re-assigning positions to all children.
+  result_bool (*internal_assign_positions)(base_widget*);
+
+  /// @brief Internal callback for rendering this widget.
+  result_bool (*internal_render_callback)(const base_widget*);
+
+  /// @brief Internal callback for processing internal mouse motion event.
+  ///        These internal callbacks are internally handled by base widget.
+  result_bool (*internal_mouse_motion_callback)(base_widget*,
+                                                internal_mouse_motion_event*);
+
+  /// @brief Internal callback for processing internal mouse button event.
+  ///        These internal callbacks are internally handled by base widget.
+  result_bool (*internal_mouse_button_callback)(base_widget*,
+                                                internal_mouse_button_event*);
+
+  /// @brief Internal callback for processing internal mouse scroll event.
+  ///        These internal callbacks are internally handled by base widget.
+  result_bool (*internal_mouse_scroll_callback)(base_widget*,
+                                                internal_mouse_scroll_event*);
+
+  /// @brief Derived widget's callback for mouse button down callback.
+  ///        It's the derived widget's responsibility for using these
+  ///        callbacks appropriately.
+  bool (*mouse_button_down_callback)(base_widget*, const mouse_button_event);
+
+  /// @brief Derived widget's callback for mouse button up callback.
+  ///        It's the derived widget's responsibility for using these
+  ///        callbacks appropriately.
+  bool (*mouse_button_up_callback)(base_widget*, const mouse_button_event);
+
+  /// @brief Derived widget's callback for mouse enter callback.
+  ///        It's the derived widget's responsibility for using these
+  ///        callbacks appropriately.
+  bool (*mouse_enter_callback)(base_widget*, const mouse_motion_event);
+
+  /// @brief Derived widget's callback for mouse leave callback.
+  ///        It's the derived widget's responsibility for using these
+  ///        callbacks appropriately.
+  bool (*mouse_leave_callback)(base_widget*, const mouse_motion_event);
+
+  /// @brief Derived widget's callback for mouse scroll callback.
+  ///        It's the derived widget's responsibility for using these
+  ///        callbacks appropriately.
+  bool (*mouse_scroll_callback)(base_widget*, const mouse_scroll_event);
+} base_widget;
+
+/// @brief Base widget child node.
+typedef struct base_widget_child_node
+{
+  /// @brief Child
+  base_widget* child;
+
+  /// @brief Next child node.
+  base_widget_child_node* next;
+} base_widget_child_node;
 
 typedef struct result_base_widget_ptr
 {
@@ -28,69 +129,37 @@ typedef struct result_base_widget_child_node_ptr
   };
 } result_base_widget_child_node_ptr;
 
-typedef enum callback_type
-{
-  PASSING_DOWN,
-  BUBBLING_UP
-} callback_type;
-
-typedef enum event_callback_type
-{
-  EVENT_CALLBACK_TYPE_MOUSE_ENTER,
-  EVENT_CALLBACK_TYPE_MOUSE_LEAVE,
-  EVENT_CALLBACK_TYPE_MOUSE_BUTTON_DOWN,
-  EVENT_CALLBACK_TYPE_MOUSE_BUTTON_UP,
-  EVENT_CALLBACK_TYPE_MOUSE_SCROLL
-} event_callback_type;
-
-struct base_widget
-{
-  int16 x, y;
-  uint16 w, h;
-  bool visible;
-  color foreground, background;
-
-  void* derived;
-  base_widget* parent;
-  base_widget_child_node* children_head;
-
-  smoll_context* context;
-
-  rect (*internal_get_bounding_rect_callback)(base_widget*);
-  result_bool (*internal_fit_layout_callback)(base_widget*);
-  result_bool (*internal_assign_positions)(base_widget*);
-  result_bool (*internal_render_callback)(const base_widget*);
-
-  result_bool (*internal_mouse_motion_callback)(base_widget*,
-                                                const mouse_motion_event,
-                                                const callback_type);
-  result_bool (*internal_mouse_button_callback)(base_widget*,
-                                                const mouse_button_event,
-                                                const callback_type);
-  result_bool (*internal_mouse_scroll_callback)(base_widget*,
-                                                const mouse_scroll_event,
-                                                const callback_type);
-
-  bool (*mouse_button_down_callback)(base_widget*, const mouse_button_event);
-  bool (*mouse_button_up_callback)(base_widget*, const mouse_button_event);
-  bool (*mouse_enter_callback)(base_widget*, const mouse_motion_event);
-  bool (*mouse_leave_callback)(base_widget*, const mouse_motion_event);
-  bool (*mouse_scroll_callback)(base_widget*, const mouse_scroll_event);
-};
-
-struct base_widget_child_node
-{
-  base_widget* child;
-  base_widget_child_node* next;
-};
-
+/// @brief Creates a new base widegt child node enclosing this widget.
+/// @param child the widget to create child node for.
+/// @return Base widget child node pointer result.
 result_base_widget_child_node_ptr
 base_widget_child_node_new(base_widget* child);
+
+/// @brief Frees the resources used by the child node.
+///        This doesn't free the widget it encloses.
+/// @param node pointer to child node.
+/// @return Void result.
 result_void base_widget_child_node_free(base_widget_child_node* node);
 
-result_base_widget_ptr base_widget_new(smoll_context* context);
+/// @brief Creates a new base widget.
+/// @return Base widget pointer result.
+result_base_widget_ptr base_widget_new();
+
+/// @brief Adds the given child widget to this widget.
+/// @param base pointer to base widget, for which the child is to be added.
+/// @param child pointer to base widget of the child to be added.
+/// @return Void result.
 result_void base_widget_add_child(base_widget* base, base_widget* child);
+
+/// @brief Removes the given widget from this widget's children.
+/// @param base pointer to base widget, from which the child should be removed.
+/// @param child pointer to base widget of the child.
+/// @return Void result.
 result_void base_widget_remove_child(base_widget* base, base_widget* child);
+
+/// @brief Frees the base widget.
+/// @param widget pointer to the base widget.
+/// @return Void result.
 result_void base_widget_free(base_widget* widget);
 
 #endif
--- src/base_widget.c
+++ src/base_widget.c
@@ -4,16 +4,13 @@
 static rect default_internal_get_bounding_rect_callback(base_widget* widget);
 static result_bool
 default_internal_mouse_motion_callback(base_widget* widget,
-                                       const mouse_motion_event event,
-                                       const callback_type type);
+                                       internal_mouse_motion_event* event);
 static result_bool
 default_internal_mouse_button_callback(base_widget* widget,
-                                       const mouse_button_event event,
-                                       const callback_type type);
+                                       internal_mouse_button_event* event);
 static result_bool
 default_internal_mouse_scroll_callback(base_widget* widget,
-                                       const mouse_scroll_event event,
-                                       const callback_type type);
+                                       internal_mouse_scroll_event* event);
 
 result_base_widget_child_node_ptr base_widget_child_node_new(base_widget* child)
 {
@@ -51,15 +48,8 @@ result_void base_widget_child_node_free(base_widget_child_node* node)
   return (result_void){.ok = true, .error = NULL};
 }
 
-result_base_widget_ptr base_widget_new(smoll_context* context)
+result_base_widget_ptr base_widget_new()
 {
-  if(!context)
-  {
-    return (result_base_widget_ptr){
-      .ok = false,
-      .error = "Cannot create base widget, with context pointing to NULL!"};
-  }
-
   base_widget* widget = (base_widget*)calloc(1, sizeof(base_widget));
   if(!widget)
   {
@@ -81,7 +71,7 @@ result_base_widget_ptr base_widget_new(smoll_context* context)
   widget->parent = NULL;
   widget->children_head = NULL;
 
-  widget->context = context;
+  widget->context = NULL;
 
   widget->internal_get_bounding_rect_callback =
     default_internal_get_bounding_rect_callback;
@@ -120,6 +110,8 @@ result_void base_widget_add_child(base_widget* base, base_widget* child)
       .ok = false,
       .error = "Cannot attach NULL pointed child to parent widget!"};
   }
+
+  child->context = base->context;
 
   if(!base->children_head)
   {
@@ -357,296 +349,290 @@ get_nearest_parent_with_valid_mouse_motion_callbacks(base_widget* widget)
   return (result_base_widget_ptr){.ok = true, .value = __.value};
 }
 
-result_base_widget_ptr
-get_nearest_parent_with_valid_event_callback(base_widget* widget,
-                                             const event_callback_type type)
-{
-  if(!widget)
-  {
-    return (result_base_widget_ptr){
-      .ok = false,
-      .error = "Cannot get nearest parent with valid event callback, with "
-               "widget pointing to NULL!"};
-  }
+// result_base_widget_ptr
+// get_nearest_parent_with_valid_event_callback(base_widget* widget,
+//                                              const event_callback_type type)
+// {
+//   if(!widget)
+//   {
+//     return (result_base_widget_ptr){
+//       .ok = false,
+//       .error = "Cannot get nearest parent with valid event callback, with "
+//                "widget pointing to NULL!"};
+//   }
 
-  if(type < EVENT_CALLBACK_TYPE_MOUSE_ENTER ||
-     type > EVENT_CALLBACK_TYPE_MOUSE_SCROLL)
-  {
-    return (result_base_widget_ptr){
-      .ok = false,
-      .error = "Cannot get nearest parent with valid event callback, with "
-               "invalid event callback type!"};
-  }
+//   if(type < EVENT_CALLBACK_TYPE_MOUSE_ENTER ||
+//      type > EVENT_CALLBACK_TYPE_MOUSE_SCROLL)
+//   {
+//     return (result_base_widget_ptr){
+//       .ok = false,
+//       .error = "Cannot get nearest parent with valid event callback, with "
+//                "invalid event callback type!"};
+//   }
 
-  if(!widget->parent)
-  {
-    return (result_base_widget_ptr){.ok = true, .value = NULL};
-  }
+//   if(!widget->parent)
+//   {
+//     return (result_base_widget_ptr){.ok = true, .value = NULL};
+//   }
 
-  switch(type)
-  {
-  case EVENT_CALLBACK_TYPE_MOUSE_ENTER: {
-    if(widget->parent->mouse_enter_callback)
-    {
-      return (result_base_widget_ptr){.ok = true, .value = widget->parent};
-    }
-  }
-  case EVENT_CALLBACK_TYPE_MOUSE_LEAVE: {
-    if(widget->parent->mouse_leave_callback)
-    {
-      return (result_base_widget_ptr){.ok = true, .value = widget->parent};
-    }
-  }
-  case EVENT_CALLBACK_TYPE_MOUSE_BUTTON_DOWN: {
-    if(widget->parent->mouse_button_down_callback)
-    {
-      return (result_base_widget_ptr){.ok = true, .value = widget->parent};
-    }
-  }
-  case EVENT_CALLBACK_TYPE_MOUSE_BUTTON_UP: {
-    if(widget->parent->mouse_button_up_callback)
-    {
-      return (result_base_widget_ptr){.ok = true, .value = widget->parent};
-    }
-  }
-  case EVENT_CALLBACK_TYPE_MOUSE_SCROLL: {
-    if(widget->parent->mouse_scroll_callback)
-    {
-      return (result_base_widget_ptr){.ok = true, .value = widget->parent};
-    }
-  }
-  default:
-    break;
-  }
+//   switch(type)
+//   {
+//   case EVENT_CALLBACK_TYPE_MOUSE_ENTER: {
+//     if(widget->parent->mouse_enter_callback)
+//     {
+//       return (result_base_widget_ptr){.ok = true, .value = widget->parent};
+//     }
+//   }
+//   case EVENT_CALLBACK_TYPE_MOUSE_LEAVE: {
+//     if(widget->parent->mouse_leave_callback)
+//     {
+//       return (result_base_widget_ptr){.ok = true, .value = widget->parent};
+//     }
+//   }
+//   case EVENT_CALLBACK_TYPE_MOUSE_BUTTON_DOWN: {
+//     if(widget->parent->mouse_button_down_callback)
+//     {
+//       return (result_base_widget_ptr){.ok = true, .value = widget->parent};
+//     }
+//   }
+//   case EVENT_CALLBACK_TYPE_MOUSE_BUTTON_UP: {
+//     if(widget->parent->mouse_button_up_callback)
+//     {
+//       return (result_base_widget_ptr){.ok = true, .value = widget->parent};
+//     }
+//   }
+//   case EVENT_CALLBACK_TYPE_MOUSE_SCROLL: {
+//     if(widget->parent->mouse_scroll_callback)
+//     {
+//       return (result_base_widget_ptr){.ok = true, .value = widget->parent};
+//     }
+//   }
+//   default:
+//     break;
+//   }
 
-  result_base_widget_ptr __ =
-    get_nearest_parent_with_valid_event_callback(widget->parent, type);
-  if(!__.ok)
-  {
-    return __;
-  }
+//   result_base_widget_ptr __ =
+//     get_nearest_parent_with_valid_event_callback(widget->parent, type);
+//   if(!__.ok)
+//   {
+//     return __;
+//   }
 
-  return (result_base_widget_ptr){.ok = true, .value = __.value};
-}
+//   return (result_base_widget_ptr){.ok = true, .value = __.value};
+// }
 
-result_bool default_internal_mouse_motion_callback(
-  base_widget* widget, const mouse_motion_event event, const callback_type type)
-{
-  if(!widget)
-  {
-    return (result_bool){.ok = false,
-                         .error = "Cannot process internal mouse motion "
-                                  "callback on NULL pointed base widget!"};
-  }
+// result_bool default_internal_mouse_motion_callback(
+//   base_widget* widget, const mouse_motion_event event, const callback_type type)
+// {
+//   if(!widget)
+//   {
+//     return (result_bool){.ok = false,
+//                          .error = "Cannot process internal mouse motion "
+//                                   "callback on NULL pointed base widget!"};
+//   }
 
-  if(type < PASSING_DOWN || type > BUBBLING_UP)
-  {
-    return (result_bool){.ok = false,
-                         .error = "Cannot process internal mouse motion "
-                                  "callback, with invalid callback type!"};
-  }
+//   if(type < PASSING_DOWN || type > BUBBLING_UP)
+//   {
+//     return (result_bool){.ok = false,
+//                          .error = "Cannot process internal mouse motion "
+//                                   "callback, with invalid callback type!"};
+//   }
 
-  if(type == PASSING_DOWN)
-  {
-    result_base_widget_ptr _ =
-      get_deepest_child_with_point(widget, event.x, event.y);
-    if(!_.ok)
-    {
-      return (result_bool){.ok = true, .value = false};
-    }
+//   if(type == PASSING_DOWN)
+//   {
+//     result_base_widget_ptr _ =
+//       get_deepest_child_with_point(widget, event.x, event.y);
+//     if(!_.ok)
+//     {
+//       return (result_bool){.ok = true, .value = false};
+//     }
 
-    base_widget* deepest_widget = _.value;
+//     base_widget* deepest_widget = _.value;
 
-    _ = smoll_context_get_mouse_focused_widget(widget->context);
-    if(!_.ok)
-    {
-      return (result_bool){.ok = false, .error = _.error};
-    }
+//     base_widget* mouse_focused_widget = widget->context->mouse_focused_widget;
 
-    base_widget* mouse_focused_widget = _.value;
+//     if(deepest_widget != mouse_focused_widget)
+//     {
+//       // mouse gone to new widget
+//       // should call mouse enter on this deepest widget
+//       // should call mouse leave on last mouse focused widget
+//       bool event_processed = false;
+//       if(mouse_focused_widget->mouse_leave_callback)
+//       {
+//         event_processed =
+//           event_processed || mouse_focused_widget->mouse_leave_callback(
+//                                mouse_focused_widget, event);
+//       }
+//       if(deepest_widget->mouse_enter_callback)
+//       {
+//         event_processed =
+//           event_processed ||
+//           deepest_widget->mouse_enter_callback(deepest_widget, event);
+//       }
 
-    if(deepest_widget != mouse_focused_widget)
-    {
-      // mouse gone to new widget
-      // should call mouse enter on this deepest widget
-      // should call mouse leave on last mouse focused widget
-      bool event_processed = false;
-      if(mouse_focused_widget->mouse_leave_callback)
-      {
-        event_processed =
-          event_processed || mouse_focused_widget->mouse_leave_callback(
-                               mouse_focused_widget, event);
-      }
-      if(deepest_widget->mouse_enter_callback)
-      {
-        event_processed =
-          event_processed ||
-          deepest_widget->mouse_enter_callback(deepest_widget, event);
-      }
+//       return (result_bool){.ok = true, .value = event_processed};
+//     }
 
-      return (result_bool){.ok = true, .value = event_processed};
-    }
+//     // Currently BUBBLING_UP event for mouse_motion is disabled
+//     // due to greater complexities
+//     // _ = get_nearest_parent_with_valid_mouse_motion_callbacks(deepest_widget);
+//     // if (!_.ok) {
+//     //   return (result_bool) {
+//     //     .ok = false,
+//     //     .error = _.value
+//     //   };
+//     // }
 
-    // Currently BUBBLING_UP event for mouse_motion is disabled
-    // due to greater complexities
-    // _ = get_nearest_parent_with_valid_mouse_motion_callbacks(deepest_widget);
-    // if (!_.ok) {
-    //   return (result_bool) {
-    //     .ok = false,
-    //     .error = _.value
-    //   };
-    // }
+//     // base_widget* nearest_parent_with_valid_callbacks = _.value;
 
-    // base_widget* nearest_parent_with_valid_callbacks = _.value;
+//     return (result_bool){.ok = true, .value = false};
+//   }
+//   else
+//   {
+//     // Currently BUBBLING_UP event for mouse_motion is disabled
+//     // due to greater complexities
+//   }
 
-    return (result_bool){.ok = true, .value = false};
-  }
-  else
-  {
-    // Currently BUBBLING_UP event for mouse_motion is disabled
-    // due to greater complexities
-  }
+//   return (result_bool){.ok = true, .value = false};
+// }
 
-  return (result_bool){.ok = true, .value = false};
-}
+// result_bool default_internal_mouse_button_callback(
+//   base_widget* widget, const mouse_button_event event, const callback_type type)
+// {
+//   if(!widget)
+//   {
+//     return (result_bool){.ok = true,
+//                          .error = "Cannot process internal mouse button "
+//                                   "callback, with widget pointing to NULL!"};
+//   }
 
-result_bool default_internal_mouse_button_callback(
-  base_widget* widget, const mouse_button_event event, const callback_type type)
-{
-  if(!widget)
-  {
-    return (result_bool){.ok = true,
-                         .error = "Cannot process internal mouse button "
-                                  "callback, with widget pointing to NULL!"};
-  }
+//   if(type < PASSING_DOWN || type > BUBBLING_UP)
+//   {
+//     return (result_bool){.ok = false,
+//                          .error = "Cannot process internal mouse button "
+//                                   "callback, with invalid callback type!"};
+//   }
 
-  if(type < PASSING_DOWN || type > BUBBLING_UP)
-  {
-    return (result_bool){.ok = false,
-                         .error = "Cannot process internal mouse button "
-                                  "callback, with invalid callback type!"};
-  }
+//   if(type == PASSING_DOWN)
+//   {
+//     result_base_widget_ptr _ =
+//       get_deepest_child_with_point(widget, event.x, event.y);
+//     if(!_.ok)
+//     {
+//       return (result_bool){.ok = true, .value = false};
+//     }
 
-  if(type == PASSING_DOWN)
-  {
-    result_base_widget_ptr _ =
-      get_deepest_child_with_point(widget, event.x, event.y);
-    if(!_.ok)
-    {
-      return (result_bool){.ok = true, .value = false};
-    }
+//     base_widget* deepest_widget = _.value;
 
-    base_widget* deepest_widget = _.value;
+//     if(event.button_state == MOUSE_BUTTON_DOWN)
+//     {
+//       if(deepest_widget->mouse_button_down_callback)
+//       {
+//         return (result_bool){
+//           .ok = true,
+//           .value =
+//             deepest_widget->mouse_button_down_callback(deepest_widget, event)};
+//       }
+//     }
+//     else
+//     {
+//       if(deepest_widget->mouse_button_up_callback)
+//       {
+//         return (result_bool){.ok = true,
+//                              .value = deepest_widget->mouse_button_up_callback(
+//                                deepest_widget, event)};
+//       }
+//     }
+//   }
 
-    if(event.button_state == MOUSE_BUTTON_DOWN)
-    {
-      if(deepest_widget->mouse_button_down_callback)
-      {
-        return (result_bool){
-          .ok = true,
-          .value =
-            deepest_widget->mouse_button_down_callback(deepest_widget, event)};
-      }
-    }
-    else
-    {
-      if(deepest_widget->mouse_button_up_callback)
-      {
-        return (result_bool){.ok = true,
-                             .value = deepest_widget->mouse_button_up_callback(
-                               deepest_widget, event)};
-      }
-    }
-  }
+//   // BUBBLING UP
 
-  // BUBBLING UP
+//   if(event.button_state == MOUSE_BUTTON_DOWN)
+//   {
+//     if(widget->mouse_button_down_callback)
+//     {
+//       return (result_bool){
+//         .ok = true, .value = widget->mouse_button_down_callback(widget, event)};
+//     }
+//   }
+//   else
+//   {
+//     if(widget->mouse_button_up_callback)
+//     {
+//       return (result_bool){
+//         .ok = true, .value = widget->mouse_button_up_callback(widget, event)};
+//     }
+//   }
 
-  if(event.button_state == MOUSE_BUTTON_DOWN)
-  {
-    if(widget->mouse_button_down_callback)
-    {
-      return (result_bool){
-        .ok = true, .value = widget->mouse_button_down_callback(widget, event)};
-    }
-  }
-  else
-  {
-    if(widget->mouse_button_up_callback)
-    {
-      return (result_bool){
-        .ok = true, .value = widget->mouse_button_up_callback(widget, event)};
-    }
-  }
+//   if(widget->parent)
+//   {
+//     return widget->parent->internal_mouse_button_callback(
+//       widget->parent, event, type);
+//   }
 
-  if(widget->parent)
-  {
-    return widget->parent->internal_mouse_button_callback(
-      widget->parent, event, type);
-  }
+//   return (result_bool){.ok = true, .value = false};
+// }
+// result_bool default_internal_mouse_scroll_callback(
+//   base_widget* widget, const mouse_scroll_event event, const callback_type type)
+// {
+//   if(!widget)
+//   {
+//     return (result_bool){.ok = true,
+//                          .error = "Cannot process internal mouse scroll "
+//                                   "callback, with widget pointing to NULL!"};
+//   }
 
-  return (result_bool){.ok = true, .value = false};
-}
-result_bool default_internal_mouse_scroll_callback(
-  base_widget* widget, const mouse_scroll_event event, const callback_type type)
-{
-  if(!widget)
-  {
-    return (result_bool){.ok = true,
-                         .error = "Cannot process internal mouse scroll "
-                                  "callback, with widget pointing to NULL!"};
-  }
+//   if(type < PASSING_DOWN || type > BUBBLING_UP)
+//   {
+//     return (result_bool){.ok = false,
+//                          .error = "Cannot process internal mouse scroll "
+//                                   "callback, with invalid callback type!"};
+//   }
 
-  if(type < PASSING_DOWN || type > BUBBLING_UP)
-  {
-    return (result_bool){.ok = false,
-                         .error = "Cannot process internal mouse scroll "
-                                  "callback, with invalid callback type!"};
-  }
+//   if(type == PASSING_DOWN)
+//   {
+//     result_base_widget_ptr _ = get_deepest_child_with_point(
+//       widget,
+//       smoll_context_get_mouse_x(widget->context).value,
+//       smoll_context_get_mouse_y(widget->context).value);
+//     if(!_.ok)
+//     {
+//       return (result_bool){.ok = true, .value = false};
+//     }
 
-  if(type == PASSING_DOWN)
-  {
-    result_base_widget_ptr _ = get_deepest_child_with_point(
-      widget,
-      smoll_context_get_mouse_x(widget->context).value,
-      smoll_context_get_mouse_y(widget->context).value);
-    if(!_.ok)
-    {
-      return (result_bool){.ok = true, .value = false};
-    }
+//     base_widget* deepest_widget = _.value;
 
-    base_widget* deepest_widget = _.value;
+//     if(deepest_widget->mouse_scroll_callback)
+//     {
+//       return (result_bool){
+//         .ok = true,
+//         .value = deepest_widget->mouse_scroll_callback(deepest_widget, event)};
+//     }
 
-    if(deepest_widget->mouse_scroll_callback)
-    {
-      return (result_bool){
-        .ok = true,
-        .value = deepest_widget->mouse_scroll_callback(deepest_widget, event)};
-    }
+//     // should bubble up events to parent
+//     if(!deepest_widget->parent)
+//     {
+//       return (result_bool){.ok = true, .value = false};
+//     }
 
-    // should bubble up events to parent
-    if(!deepest_widget->parent)
-    {
-      return (result_bool){.ok = true, .value = false};
-    }
+//     deepest_widget->parent->internal_mouse_scroll_callback(
+//       deepest_widget->parent, event, BUBBLING_UP);
+//   }
 
-    deepest_widget->parent->internal_mouse_scroll_callback(
-      deepest_widget->parent, event, BUBBLING_UP);
-  }
+//   // BUBBLING UP
 
-  // BUBBLING UP
+//   if(!widget->mouse_scroll_callback)
+//   {
+//     // as this widget doesn't have scroll callback
+//     // further bubble up this event to parents
+//     if(!widget->parent)
+//     {
+//       return (result_bool){.ok = true, .value = false};
+//     }
 
-  if(!widget->mouse_scroll_callback)
-  {
-    // as this widget doesn't have scroll callback
-    // further bubble up this event to parents
-    if(!widget->parent)
-    {
-      return (result_bool){.ok = true, .value = false};
-    }
+//     widget->parent->internal_mouse_scroll_callback(widget->parent, event, type);
+//   }
 
-    widget->parent->internal_mouse_scroll_callback(widget->parent, event, type);
-  }
-
-  return (result_bool){.ok = true,
-                       .value = widget->mouse_scroll_callback(widget, event)};
-}
+//   return (result_bool){.ok = true,
+//                        .value = widget->mouse_scroll_callback(widget, event)};
+// }
