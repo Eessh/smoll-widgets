2830224c6a7434261405d729dd2cbfdcd6530c5a
Eessh <eesshkumarjagana@gmail.com>
2023-08-07 15:00:16 +0530

    added: win32 + cairo example project in premake5 script

--- premake5.lua
+++ premake5.lua
@@ -51,3 +51,32 @@ workspace("smoll-widgets")
 			    "bin/Release" -- smoll-widgets built library path
 			})
 		filter({})
+
+	-- Win32 + Cairo example
+    project("win32-cairo-example")
+        language("C")
+        includedirs({
+            "include",
+            "backends/win32_cairo",
+        })
+        files({
+            "backends/win32_cairo/*.c",
+        })
+        links({
+            "gdi32",
+            "cairo",
+            "smoll-widgets"
+        })
+        filter("configurations:Debug")
+            kind("ConsoleApp")
+            libdirs({
+                "backends/win32_cairo/cairo-windows-1.17.2/lib/x64",
+                "bin/Debug" -- smoll-widgets built library path
+            })
+        filter("configurations:Release")
+            kind("WindowedApp")
+            libdirs({
+                "backends/win32_cairo/cairo-windows-1.17.2/lib/x64",
+                "bin/Release" -- smoll-widgets built library path
+            })
+        filter({})
